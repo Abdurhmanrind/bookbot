06160d40586bdcf91772b206ec5c78b56799dfb6
dadagi <abdurhmanrind@gmail.com>
2025-07-12 20:13:49 +0300

    Completing bookbot project

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
+import sys
 from stats import get_book_word_num
+from stats import get_chara_num
+from stats import sorting_list
 def get_book_text(filepath):
     with open(filepath) as file:
         file_contents = file.read()
@@ -6,8 +9,23 @@ def get_book_text(filepath):
 
 
 def main():
-    book = get_book_text("books/frankenstein.txt")
-    num_words = get_book_word_num(book)
-    print(f"{num_words} words found in the document")
+    if len(sys.argv) == 2:
+        file_path = sys.argv[1]
+        book = get_book_text(file_path)
+        num_words = get_book_word_num(book)
+        all_charac = get_chara_num(book)
+        sorted_list , list_num = sorting_list(all_charac)
+        sorted_list.sort(key=lambda item: item["num"], reverse=True)
+        print("============ BOOKBOT ============")
+        print(f"Analyzing book found at {file_path}...")
+        print("----------- Word Count ----------")
+        print(f"Found {num_words} total words")
+        print("--------- Character Count -------")
+        for item in sorted_list:
+            print(f"{item['char']}: {item['num']}")
+        print("============= END ===============")
+    else:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
 
 main()
--- stats.py
+++ stats.py
@@ -1,3 +1,28 @@
 def get_book_word_num(book_contents):
     list_words = book_contents.split()
     return len(list_words)
+#---------------------------------------------------
+
+def get_chara_num(book_contents):
+    all_charac = {}
+    all_lower_text = book_contents.lower()
+    for character in all_lower_text:
+        if character not in all_charac:
+            all_charac[character] = 1
+        else:
+            all_charac[character] += 1
+    return all_charac
+#---------------------------------------------------
+
+def sorting_list(unsorted_dic):
+    sorted_list = []
+    list_num = []
+    for i in unsorted_dic:
+        if i.isalpha():
+            each_dic = {}
+            each_dic["char"] = i
+            each_dic["num"] = unsorted_dic[i]
+            sorted_list.append(each_dic)
+            list_num.append(unsorted_dic[i])
+
+    return sorted_list , list_num
